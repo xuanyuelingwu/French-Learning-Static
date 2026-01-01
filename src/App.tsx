aabfbaaefdcdec8ad65530739e942fdc88b2f531
xuanyuelingwu <182743256+xuanyuelingwu@users.noreply.github.com>
2026-01-01 11:40:56 -0500

    Fix build errors: remove tooltip dependency and tw-animate-css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
@@ -32,10 +32,8 @@ function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light">
-        <TooltipProvider>
           <Toaster />
           <Router />
-        </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
   );
